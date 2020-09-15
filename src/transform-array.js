44f32b4b5d14ac1e4e86420b58655a8380dab2b8
Artur <arturexpone@yandex.ru>
2020-09-15 20:13:17 +0300

    feat: resolve transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('err');
+  const sortArray = [];
+  arr.forEach((value, i) => {
+    switch(value) {
+      case '--discard-next':
+      case '--discard-prev':
+      case '--double-next':
+      case '--double-prev':
+        return;
+    }
+
+    const decrement = arr[i - 1];
+    const increment = arr[i + 1];
+
+    if (decrement === '--discard-next') return;
+    if (decrement === '--double-next') {
+      sortArray.push(value, value)
+    } else {
+      sortArray.push(value);
+    }
+
+    if (increment === '--double-prev') {
+      sortArray.push(value);
+    } else if (increment === '--discard-prev') {
+      sortArray.length = sortArray.length - 1;
+    }
+
+  })
+  return sortArray;
 };
