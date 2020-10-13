630b3042e648ece6b13a096bd8f347c93dc6a480
arturexpone <arturexpone@yandex.ru>
2020-10-13 10:47:27 +0300

    feat: resolve extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if(!options) {
+    return str.toString();
+  }
+  const newStr = str + '';
+  const {repeatTimes = 0, separator = '+', addition ='', additionRepeatTimes = 0, additionSeparator = '|'} = options;
+  const newAddition = addition + '';
+  let resultAdditional = '';
+  let resultString = '';
+
+  if(additionRepeatTimes === 0 || additionRepeatTimes === 1) {
+    resultAdditional += newAddition;
+  }else if(additionRepeatTimes > 1) {
+    for(let i = 0; i < additionRepeatTimes; i++) {
+      if((i + 1) !== additionRepeatTimes) {
+        resultAdditional += newAddition + additionSeparator
+      }else if ((i + 1) === additionRepeatTimes) {
+        resultAdditional += newAddition
+      }
+    }
+  }
+
+  if (repeatTimes === 0 || repeatTimes === 1) {
+    resultString += newStr + resultAdditional;
+  }else if(repeatTimes > 1) {
+    for (let i = 0; i < repeatTimes; i++) {
+      if((i + 1) !== repeatTimes) {
+        resultString += newStr + resultAdditional + separator;
+      }else if((i + 1) === repeatTimes) {
+        resultString += newStr + resultAdditional;
+      }
+    }
+  }
+  return resultString;
 };
   
