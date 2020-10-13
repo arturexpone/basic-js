d4a39f31cebd7576799e2ed36b5028d5990da411
arturexpone <arturexpone@yandex.ru>
2020-10-13 10:51:52 +0300

    feat: resolve vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(data = true) {
+    this.data = data;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+  encrypt(message, key) {
+    if(!message || !key) throw new Error();
+    const maxLength = message.length;
+    let resultArr = [];
+    let step = -1;
+    for (let i = 0; i < maxLength; i++) {
+      if (this.alphabet.indexOf(message[i].toUpperCase()) > -1) {
+        step++;
+        let num = (this.alphabet.indexOf(message[i].toUpperCase()) + this.alphabet.indexOf(key[step % key.length].toUpperCase())) % this.alphabet.length;
+        resultArr = [...resultArr, ...this.alphabet[num]];
+      } else {
+        resultArr = [...resultArr, ...message[i]];
+      }
+    }
+    return this.data ? resultArr.join('') : resultArr.reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if(!message || !key) throw new Error();
+    const maxLength = message.length;
+    let resultArr = [];
+    let step = -1;
+    for (let i = 0; i < maxLength; i++) {
+      if (this.alphabet.indexOf(message[i].toUpperCase()) > -1) {
+        step++;
+        let num = (this.alphabet.indexOf(message[i].toUpperCase()) + this.alphabet.length - this.alphabet.indexOf(key[step % key.length].toUpperCase())) % this.alphabet.length;
+        resultArr = [...resultArr, ...this.alphabet[num]];
+      } else {
+        resultArr = [...resultArr, ...message[i]];
+      }
+    }
+    return this.data ? resultArr.join('') : resultArr.reverse().join('');
   }
 }
 
